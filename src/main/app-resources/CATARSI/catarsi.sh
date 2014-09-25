e76deae85dcc476a4d4e7b44c4514544dabaf385
fdandria <fdandria@vm-10-10-31-51.dev.terradue.int>
2014-09-25 15:56:00 +0200

    [LAI] first working version

---
+++ src/main/app-resources/CATARSI/catarsi.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#       Copyright 1995-2008, Advanced Computer Systems , Inc.
+#       Via Della Bufalotta, 378 - 00139 Roma - Italy
+#       http://www.acsys.it
+#
+#       All Rights Reserved.
+#
+#       This is UNPUBLISHED PROPRIETARY SOURCE CODE of Advanced Computer Systems;
+#       the contents of this file may not be disclosed to third parties, copied or
+#       duplicated in any form, in whole or in part, without the prior written
+#       permission of Advanced Computer Systems, Inc.
+#
+#       $Author: paolo.castracane $
+#
+#       $Prod: A.C.S. tools $
+#
+#       $Id:  $
+#
+#   $Log: $
+#
+#
+#
+#project=CATARSI
+
+export ROOT_DIR=/application
+export CATA_ROOT_DIR=$ROOT_DIR/CATARSI 
+#export CATA_IDL_CODE=$CATA_ROOT_DIR/IdlCode
+export CATA_SAV=$CATA_ROOT_DIR/batch_Processor/sav
+export CATA_WORKDIR=$CATA_ROOT_DIR/WorkDir
+
+# rm -fr /tmp/*
--- src/main/app-resources/lai/run.sh
+++ src/main/app-resources/lai/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Project:       ${project.name}
-# Author:        $Author: fbrito $ (Terradue Srl)
-# Last update:   ${doc.timestamp}:
-# Element:       ${project.name}
-# Context:       ${project.artifactId}
-# Version:       ${project.version} (${implementation.build})
-# Description:   ${project.description}
+# Project:		 ${project.name}
+# Author:		  $Author: fbrito $ (Terradue Srl)
+# Last update:	${doc.timestamp}:
+# Element:		 ${project.name}
+# Context:		 ${project.artifactId}
+# Version:		 ${project.version} (${implementation.build})
+# Description:	${project.description}
 #
 # This document is the property of Terradue and contains information directly
 # resulting from knowledge and experience of Terradue.
@@ -28,16 +28,60 @@ ERR_NOINPUT=1
 # add a trap to exit gracefully
 function cleanExit ()
 {
-   local retval=$?
-   local msg=""
-   case "$retval" in
-     $SUCCESS)          msg="Processing successfully concluded";;
-     $ERR_NOINPUT) msg="Input not retrieved to local node";;
-     *)                 msg="Unknown error";;
-   esac
-   [ "$retval" != "0" ] && ciop-log "ERROR" "Error $retval - $msg, processing aborted" || ciop-log "INFO" "$msg"
-   exit $retval
+	local retval=$?
+	local msg=""
+	case "$retval" in
+		$SUCCESS) 	msg="Processing successfully concluded";;
+		$ERR_NOINPUT)	msg="Input not retrieved to local node";;
+		*)		msg="Unknown error";;
+	esac
+
+	[ "$retval" != "0" ] && ciop-log "ERROR" "Error $retval - $msg, processing aborted" || ciop-log "INFO" "$msg"
+
+	exit $retval
 }
+
 trap cleanExit EXIT
 
+ciop-log "DEBUG" "Input dir: ${TMPDIR}/input"
+ciop-log "DEBUG" "Ouput dir: ${TMPDIR}/output"
 
+inputDir=${TMPDIR}/input
+outputDir=${TMPDIR}/output
+
+mkdir -p $inputDir
+mkdir -p $outputDir
+
+#export DISPLAY=:99
+ciop-log "DEBUG" "Running on display $DISPLAY"
+
+
+while read input
+do
+	ciop-log "INFO" "Working with file $input"
+	file=`ciop-copy -o $inputDir -U $input`
+
+	ciop-log "DEBUG" "ciop-copy output is $file"
+
+	file=`basename $file`
+
+	#call the lai su $file
+	cd $inputDir; unzip $file; rm -f $file; cd `echo $file | sed 's#\.zip##g'`
+
+	ciop-log "DEBUG" "File is $file"
+
+	myInputDir="$inputDir/`echo $file | sed 's#\.zip##g'`/"
+	myOutputDir="$outputDir/`echo $file | sed 's#\.zip##g' | sed 's#input#output#g'`/"; mkdir -p $myOutputDir
+
+	ciop-log "INFO" "Starting lai processor [/bin/sh /application/CATARSI/batch_Processor/FAPAR_and_LAI/run_lai.sh /usr/local/MATLAB/MATLAB_Compiler_Runtime/v76/ $myInputDir $myOutputDir]"
+
+	cd /application/CATARSI/batch_Processor/FAPAR_and_LAI/
+	/bin/sh -x run_LAI.sh /usr/local/MATLAB/MATLAB_Compiler_Runtime/v76/ $myInputDir $myOutputDir &> $myOutputDir/output.txt
+
+	cd $outputDir
+	tar cvfz `echo $file | sed 's#\.zip##g' | sed 's#input#output#g'`.tgz `echo $file | sed 's#\.zip##g' | sed 's#input#output#g'`
+	ls -l
+	ciop-log "INFO" "Publishing output [`echo $file | sed 's#\.zip##g' | sed 's#input#output#g'`]"
+	ciop-publish -m $outputDir/`echo $file | sed 's#\.zip##g' | sed 's#input#output#g'`.tgz
+
+done
